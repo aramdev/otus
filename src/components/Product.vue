38c5e39ae14ceea872ed0f9bfdf4c4e9fae94cac
aram <aram.dev82@gmail.com>
2023-05-31 14:21:50 +0400

    add product

---
+++ src/components/Product.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card">
+    <span class="badge bg-primary mb-3">category: {{ category }}</span> 
+    <img :src="image" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ title }}
+      </h5>
+      <p class="card-text">
+        {{ description }}
+      </p>
+      <span class="badge bg-info text-dark">id: {{ id }}</span> |
+      <span class="badge bg-info text-dark">price: {{ price }}</span> |
+      <span class="badge bg-info text-dark">coun: {{ rating.rate }}</span> |
+      <span class="badge bg-info text-dark">coun: {{ rating.count }}</span>
+      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+.card
+  height: 100%
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <!-- <pre> 
+      {{ products[0] }}
+    </pre> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-3 mb-3 mt-3" v-for="(product, i) in products" :key="i">
+          <Product v-bind="product"/>
+        </div>
+      </div>
+    </div>
+    <!-- <TheWelcome /> -->
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import Product from '@/components/Product.vue'
+const products = ref(null)
+const getProducts = async () => {
+  const res = await fetch('https://fakestoreapi.com/products')
+  products.value = await res.json()
+}
+onMounted(() => {
+  getProducts()
+})
+</script>
